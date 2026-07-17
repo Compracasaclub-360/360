9af6adeacea4d865fdc532d45eb4719f0803803b
Compracasaclub-360 <compracasa.club@gmail.com>
2026-07-16 19:54:57 -0600

    San sebastian 1300

---
+++ residencial-san-sebastian-1300/data.js
@@ -0,0 +1,733 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-patio-frontal",
+      "name": "Patio frontal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3247769741285733,
+        "pitch": -0.10258370369317049,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8376879192605209,
+          "pitch": -0.07305398253281759,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": -0.4144040782265339,
+          "pitch": -0.07845486062405094,
+          "rotation": 0,
+          "target": "3-comedor"
+        },
+        {
+          "yaw": -0.4046215688649202,
+          "pitch": -0.42248443291491533,
+          "rotation": 0,
+          "target": "6-recmara-1"
+        },
+        {
+          "yaw": -0.85540004428222,
+          "pitch": -0.37043328927160957,
+          "rotation": 0,
+          "target": "8-recmara-2"
+        },
+        {
+          "yaw": 1.2478946685097458,
+          "pitch": -0.05267755334862656,
+          "rotation": 0,
+          "target": "10-rea-de-juegos"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.2247061257895329,
+        "pitch": 0.1314797344298615,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6117170540231136,
+          "pitch": -0.08108668948475817,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": 0.8670300835553881,
+          "pitch": -0.03074685162237145,
+          "rotation": 0,
+          "target": "9-medio-bao"
+        },
+        {
+          "yaw": 1.1833078127272625,
+          "pitch": -0.0276067158443567,
+          "rotation": 0,
+          "target": "4-patio-trasero"
+        },
+        {
+          "yaw": 2.1777278046318536,
+          "pitch": 0.49412417431431876,
+          "rotation": 0,
+          "target": "3-comedor"
+        },
+        {
+          "yaw": -2.305390342443358,
+          "pitch": -0.019391768809104093,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        },
+        {
+          "yaw": 0.6082578533740595,
+          "pitch": -0.31659283412629335,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.1816689808501923,
+        "pitch": 0.11026542817204543,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0056437716971764,
+          "pitch": -0.1805679785368497,
+          "rotation": 0,
+          "target": "9-medio-bao"
+        },
+        {
+          "yaw": 3.0165572472629485,
+          "pitch": -0.15406154449488518,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        },
+        {
+          "yaw": -2.3900263942381024,
+          "pitch": -0.14152544987198468,
+          "rotation": 0,
+          "target": "4-patio-trasero"
+        },
+        {
+          "yaw": 1.4039635144531992,
+          "pitch": -0.02957634008449972,
+          "rotation": 0,
+          "target": "3-comedor"
+        },
+        {
+          "yaw": 2.42760991398759,
+          "pitch": -0.020827733450989427,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": 2.0279828446529136,
+          "pitch": -0.05722699551291299,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-comedor",
+      "name": "Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.697459958123064,
+        "pitch": 0.08894251147755483,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0624020629392916,
+          "pitch": 0.07111364267820086,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": 2.2309629289599684,
+          "pitch": -0.20924208883986672,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        },
+        {
+          "yaw": 2.70085452198101,
+          "pitch": -0.11479659438961498,
+          "rotation": 0,
+          "target": "4-patio-trasero"
+        },
+        {
+          "yaw": 1.829848345313776,
+          "pitch": 0.38668021956295107,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": 1.2701281939106757,
+          "pitch": 0.024802609003462095,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-patio-trasero",
+      "name": "Patio trasero",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.5615549997493687,
+        "pitch": 0.1899005211249385,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.646294992521211,
+          "pitch": 0.04886131408312444,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": 3.0486834472740414,
+          "pitch": -0.17847785765875912,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": -2.6395122546278653,
+          "pitch": -0.6596875310889683,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bao",
+      "name": "Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.4232125555909256,
+        "pitch": 1.2078838610113038,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0603941184849752,
+          "pitch": 0.7530572278673997,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        },
+        {
+          "yaw": -2.8621287641170596,
+          "pitch": 0.005653357027302519,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": 2.8267606079096073,
+          "pitch": -0.007318459806990418,
+          "rotation": 0,
+          "target": "8-recmara-2"
+        },
+        {
+          "yaw": 2.498899086203995,
+          "pitch": -0.03902785650633689,
+          "rotation": 0,
+          "target": "6-recmara-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-recmara-1",
+      "name": "Recámara 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.665671810784115,
+        "pitch": 0.3503269748420941,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.390783973325533,
+          "pitch": 0.039308230225740814,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        },
+        {
+          "yaw": -3.0573803044301044,
+          "pitch": 0.039430132705092547,
+          "rotation": 0,
+          "target": "8-recmara-2"
+        },
+        {
+          "yaw": 1.1019540076983247,
+          "pitch": 0.47701680408245295,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        },
+        {
+          "yaw": -2.094279308957942,
+          "pitch": 0.03134308224478133,
+          "rotation": 0,
+          "target": "5-bao"
+        },
+        {
+          "yaw": -2.708340148749656,
+          "pitch": 0.05209310299125747,
+          "rotation": 0,
+          "target": "1-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-primer-piso",
+      "name": "Primer piso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.703402793545994,
+        "pitch": 0.08908892474036634,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.503488107309268,
+          "pitch": 0.00030654126067020115,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": -1.7983346748367044,
+          "pitch": -0.002803855164724922,
+          "rotation": 0,
+          "target": "4-patio-trasero"
+        },
+        {
+          "yaw": 3.120799513758241,
+          "pitch": -0.021627111838899538,
+          "rotation": 0,
+          "target": "8-recmara-2"
+        },
+        {
+          "yaw": 2.3005659420063296,
+          "pitch": 0.01230827831642145,
+          "rotation": 0,
+          "target": "6-recmara-1"
+        },
+        {
+          "yaw": 0.6150963692046147,
+          "pitch": 0.3760868140825657,
+          "rotation": 0,
+          "target": "5-bao"
+        },
+        {
+          "yaw": -0.8180005550515173,
+          "pitch": -0.07004894714726362,
+          "rotation": 0,
+          "target": "6-recmara-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-recmara-2",
+      "name": "Recámara 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.1291687853641044,
+        "pitch": 0.0233743192242013,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8076323802627279,
+          "pitch": 0.0851025018654461,
+          "rotation": 0,
+          "target": "7-primer-piso"
+        },
+        {
+          "yaw": -1.6419051686383934,
+          "pitch": 0.0687860172780752,
+          "rotation": 0,
+          "target": "5-bao"
+        },
+        {
+          "yaw": -1.0947180402610126,
+          "pitch": 0.0442144390345085,
+          "rotation": 0,
+          "target": "6-recmara-1"
+        },
+        {
+          "yaw": -2.581810011109951,
+          "pitch": 0.03467397126197369,
+          "rotation": 0,
+          "target": "2-cocina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-medio-bao",
+      "name": "Medio baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4978181865278817,
+        "pitch": 0.4862953429374528,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3490729243830017,
+          "pitch": 0.2629487410233011,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": 2.7317860383432926,
+          "pitch": 0.0465651101025486,
+          "rotation": 0,
+          "target": "3-comedor"
+        },
+        {
+          "yaw": 0.9379015167118219,
+          "pitch": 0.045971692152456,
+          "rotation": 0,
+          "target": "4-patio-trasero"
+        },
+        {
+          "yaw": -2.3828022817816077,
+          "pitch": 0.13789619326478686,
+          "rotation": 0,
+          "target": "1-sala"
+        },
+        {
+          "yaw": -0.8645398770545683,
+          "pitch": 0.12168000935990264,
+          "rotation": 0,
+          "target": "1-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6929594611710783,
+          "pitch": 0.11892966348035827,
+          "title": "Regresar a la sala",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-rea-de-juegos",
+      "name": "Área de Juegos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.18528883831496046,
+        "pitch": 0.030171677556813492,
+        "fov": 1.20384993451692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8541166065244425,
+          "pitch": -0.12946093128119784,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        },
+        {
+          "yaw": -2.1801648244159875,
+          "pitch": -0.0555886548587754,
+          "rotation": 0,
+          "target": "0-patio-frontal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9767259308115062,
+          "pitch": -0.1108140372804094,
+          "title": "Regresar a casa",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.007062697386596,
+          "pitch": -0.07226665556013145,
+          "title": "Regresar a casa",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Residencial San Sebastian 1300",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
